3dc0f9dde3725d601f050d9494689a766b5abbb0
Emile Fugulin <code@efugulin.com>
2025-11-18 23:37:46 -0500

    Change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defaultConfig } from "@caido/eslint-config";
 
-/** @type {import('eslint').Linter.Config } */
 export default [
-  ...defaultConfig(),
-]
+  ...defaultConfig({
+    compat: false,
+  }),
+];
